7a50962bcabe266088f2499676e72df20db43186
ibnugp01 <ibnu.gunawan@studioalva.co>
2022-05-12 14:12:49 +0700

    add fitur

--- src/Monthly/MonthlyBody.tsx
+++ src/Monthly/MonthlyBody.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode, useContext } from 'react';
 import { useMonthlyCalendar } from './MonthlyCalendar';
 import { daysInWeek } from '../shared';
-import { format, getDay, Locale } from 'date-fns';
+import { format, getDay, isSameDay, Locale } from 'date-fns';
 
 const MonthlyBodyContext = React.createContext({} as any);
 type BodyState<DayData> = {
@@ -81,7 +81,7 @@ export function MonthlyBody<DayData>({
   let headingClassName =
     'rc-border-b-2 rc-p-2 rc-border-r-2 lg:rc-block rc-hidden';
   return (
-    <div className="rc-bg-white rc-border-l-2 rc-border-t-2">
+    <div className="rc-bg-white rc-border-l-2 rc-border-t-2 rc-rounded">
       <div
         className={`rc-grid rc-grid-cols-1 sm:rc-grid-cols-2 md:rc-grid-cols-4 ${
           //@ts-ignore
@@ -122,8 +122,9 @@ export function MonthlyBody<DayData>({
 
 type MonthlyDayProps<DayData> = {
   renderDay: (events: DayData[]) => ReactNode;
+  renderClick?: (day: Date) => ReactNode;
 };
-export function MonthlyDay<DayData>({ renderDay }: MonthlyDayProps<DayData>) {
+export function MonthlyDay<DayData>({ renderDay, renderClick }: MonthlyDayProps<DayData>) {
   let { locale } = useMonthlyCalendar();
   let { day, events } = useMonthlyBody<DayData>();
   let dayNumber = format(day, 'd', { locale });
@@ -131,15 +132,20 @@ export function MonthlyDay<DayData>({ renderDay }: MonthlyDayProps<DayData>) {
   return (
     <div
       aria-label={`Events for day ${dayNumber}`}
-      className="rc-h-48 rc-p-2 rc-border-b-2 rc-border-r-2"
+      className="md:rc-h-36 rc-p-2 rc-border-b-2 rc-border-r-2"
     >
       <div className="rc-flex rc-justify-between">
-        <div className="rc-font-bold">{dayNumber}</div>
+        <div className='rc-flex rc-flex-row rc-justify-between rc-w-full'>
+          <div className={`rc-font-bold rc-mb-2 ${isSameDay(day, new Date) ? "rc-bg-blue-500 rc-text-white rc-px-2 rc-rounded" : ""}`}>
+              {dayNumber}
+          </div>
+          {renderClick ? renderClick(day) : ''}
+        </div>
         <div className="lg:rc-hidden rc-block">
           {format(day, 'EEEE', { locale })}
         </div>
       </div>
-      <ul className="rc-divide-gray-200 rc-divide-y rc-overflow-hidden rc-max-h-36 rc-overflow-y-auto">
+      <ul className="rc-divide-gray-200 rc-divide-y rc-overflow-hidden rc-max-h-36 md:max-h-24 rc-overflow-y-auto">
         {renderDay(events)}
       </ul>
     </div>
