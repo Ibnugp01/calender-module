14c90d0fcd33d058dae2afcecaa2d294682ce878
notme1001 <igp890@outlook.com>
2022-05-17 20:05:25 +0700

    new calendar

--- example/index.tsx
+++ example/index.tsx
@@ -5,7 +5,7 @@ import * as ReactDOM from 'react-dom';
 import { events } from '../stories/dummyEvents';
 import {
   MonthlyCalendar,
-  MonthlyNav,
+  MonthlyDay,
   MonthlyBody,
   DefaultMonthlyEventItem,
 } from '../src';
@@ -15,33 +15,86 @@ export const MyMonthlyCalendar = () => {
   let [currentMonth, setCurrentMonth] = useState<Date>(
     startOfMonth(new Date())
   );
-  let eventItems = events[currentMonth.toISOString()];
+  
+  const [calendarList, setCalendarList] = useState<any | []>([
+    {
+      id: '1',
+      name: 'meeting requirement 01',
+      description: 'meeting di kantor',
+      startTime: '09:00',
+      endTime: '11:30',
+      startDate: new Date('2022-05-09T00:00:00'),
+      endDate: new Date('2022-05-10T00:00:00'),
+      date: new Date('2022-05-09T00:00:00')
+    },
+    {
+      id: '2',
+      name: 'meeting requirement 02',
+      description: 'meeting di kantor',
+      startTime: '09:00',
+      endTime: '11:30',
+      startDate: new Date('2022-05-10T00:00:00'),
+      endDate: new Date('2022-05-10T00:00:00'),
+      date: new Date('2022-05-09T00:00:00')
+    },
+    {
+      id: '3',
+      name: 'meeting requirement 03',
+      description: 'meeting di kantor',
+      startTime: '09:00',
+      endTime: '11:30',
+      startDate: new Date('2022-05-10T00:00:00'),
+      endDate: new Date('2022-05-11T00:00:00'),
+      date: new Date('2022-05-09T00:00:00')
+    }
+  ])
 
   return (
     <MonthlyCalendar
       currentMonth={currentMonth}
-      onCurrentMonthChange={date => setCurrentMonth(date)}
+      onCurrentMonthChange={(date) => setCurrentMonth(date)}
     >
-      <MonthlyNav />
-      <MonthlyBody
-        events={eventItems}
-        renderDay={data =>
-          data.map((item, index) => (
-            <DefaultMonthlyEventItem
-              key={index}
-              title={item.title}
-              date={format(item.date, 'k:mm')}
-            />
-          ))
-        }
-      />
+      <MonthlyBody events={calendarList}>
+        <MonthlyDay
+          customRender={(day, dayNumber) => (
+            <p
+              style={{ padding: 10, backgroundColor: '#eee' }}
+              onClick={() => console.log(day)}
+            >
+              {dayNumber}
+            </p>
+          )}
+          renderDay={(data: any) => {
+            return data.map((item, index) => (
+              <div
+                key={index}
+                className={`p-2 rounded mb-2 hover:opacity-90 w-full cursor-pointer text-gray-600`}
+                // onClick={() => {
+                //   setActive(!active);
+                //   setDataSelect(item);
+                // }}
+              >
+                <p className="text-sm">{item?.name}</p>
+                <p className="text-sm">
+                  {item.startTime} - {item.endTime}
+                </p>
+              </div>
+              // <DefaultMonthlyEventItem
+              //   key={index}
+              //   title={item.name}
+              //   // Format the date here to be in the format you prefer
+              //   date={format(item.date, 'k:mm')}/>
+            ));
+          }}
+        />
+      </MonthlyBody>
     </MonthlyCalendar>
   );
 };
 
 const App = () => {
   return (
-    <div>
+    <div style={{margin: 'auto', padding: 20}}>
       <MyMonthlyCalendar />
     </div>
   );
--- src/Monthly/MonthlyBody.tsx
+++ src/Monthly/MonthlyBody.tsx
@@ -1,92 +1,92 @@
-import React, { ReactNode, useContext } from 'react';
-import { useMonthlyCalendar } from './MonthlyCalendar';
-import { daysInWeek } from '../shared';
-import { format, getDay, isSameDay, Locale } from 'date-fns';
+import React, { ReactNode, useContext } from 'react'
+import { useMonthlyCalendar } from './MonthlyCalendar'
+import { daysInWeek } from '../shared'
+import { format, getDay, getDaysInMonth, isSameDay, Locale } from 'date-fns'
 
-const MonthlyBodyContext = React.createContext({} as any);
+const MonthlyBodyContext = React.createContext({} as any)
 type BodyState<DayData> = {
-  day: Date;
-  events: DayData[];
-};
+  day: Date
+  events: DayData[]
+}
 
 export function useMonthlyBody<DayData>() {
-  return useContext<BodyState<DayData>>(MonthlyBodyContext);
+  return useContext<BodyState<DayData>>(MonthlyBodyContext)
 }
 
 type OmittedDaysProps = {
-  days: Date[];
-  omitDays?: number[];
-  locale?: Locale;
-};
+  days: Date[]
+  omitDays?: number[]
+  locale?: Locale
+}
 
 export const handleOmittedDays = ({
   days,
   omitDays,
-  locale,
+  locale
 }: OmittedDaysProps) => {
-  let headings = daysInWeek({ locale });
-  let daysToRender = days;
+  let headings = daysInWeek({ locale })
+  let daysToRender = days
 
   //omit the headings and days of the week that were passed in
   if (omitDays) {
-    headings = daysInWeek({ locale }).filter(
-      day => !omitDays.includes(day.day)
-    );
-    daysToRender = days.filter(day => !omitDays.includes(getDay(day)));
+    headings = daysInWeek({ locale }).filter(day => !omitDays.includes(day.day))
+    daysToRender = days.filter(day => !omitDays.includes(getDay(day)))
   }
 
   // omit the padding if an omitted day was before the start of the month
-  let firstDayOfMonth = getDay(daysToRender[0]) as number;
+  let firstDayOfMonth = getDay(daysToRender[0]) as number
   if (omitDays) {
-    let subtractOmittedDays = omitDays.filter(day => day < firstDayOfMonth)
-      .length;
-    firstDayOfMonth = firstDayOfMonth - subtractOmittedDays;
+    let subtractOmittedDays = omitDays.filter(
+      day => day < firstDayOfMonth
+    ).length
+    firstDayOfMonth = firstDayOfMonth - subtractOmittedDays
   }
-  let padding = new Array(firstDayOfMonth).fill(0);
+  let padding = new Array(firstDayOfMonth).fill(0)
 
-  return { headings, daysToRender, padding };
-};
+  return { headings, daysToRender, padding }
+}
 
 //to prevent these from being purged in production, we make a lookup object
-const headingClasses = {
-  l3: 'lg:rc-grid-cols-2',
-  l4: 'lg:rc-grid-cols-3',
-  l5: 'lg:rc-grid-cols-4',
-  l6: 'lg:rc-grid-cols-5',
-  l7: 'lg:rc-grid-cols-7',
-};
+// const headingClasses = {
+//   l3: 'lg:rc-grid-cols-2',
+//   l4: 'lg:rc-grid-cols-3',
+//   l5: 'lg:rc-grid-cols-4',
+//   l6: 'lg:rc-grid-cols-5',
+//   l7: 'lg:rc-grid-cols-7'
+// }
 
 type MonthlyBodyProps<DayData> = {
   /*
     skip days, an array of days, starts at sunday (0), saturday is 6
     ex: [0,6] would remove sunday and saturday from rendering
   */
-  omitDays?: number[];
-  events: (DayData & { date: Date, endDate: Date, startDate: Date })[];
-  children: ReactNode;
-};
+  omitDays?: number[]
+  events: (DayData & { date: Date; endDate: Date; startDate: Date })[]
+  children: ReactNode
+}
 
 export function MonthlyBody<DayData>({
   omitDays,
   events,
-  children,
+  children
 }: MonthlyBodyProps<DayData>) {
-  let { days, locale } = useMonthlyCalendar();
+  let { days, locale } = useMonthlyCalendar()
   let { headings, daysToRender, padding } = handleOmittedDays({
     days,
     omitDays,
-    locale,
-  });
+    locale
+  })
 
-  let headingClassName =
-    'rc-text-center rc-p-2 rc-border-r-2 lg:rc-block rc-hidden';
+  let underMonth = padding.map((_, index) => getDaysInMonth(new Date(days[0].getFullYear(), days[0].getMonth()-1)) - index).reverse()
+
+  let mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+
+  let headingClassName = 'rc-text-center rc-p-2 lg:rc-block rc-font-semibold'
+  let paddingClassName = 'rc-text-center rc-p-2 lg:rc-block rc-text-gray-600'
   return (
-    <div className="rc-bg-white rc-border-l-2 rc-border-t-2 rc-rounded">
+    <div className="rc-bg-white rc-rounded">
       <div
-        className={`rc-grid rc-grid-cols-1 sm:rc-grid-cols-2 md:rc-grid-cols-4 rc-overflow-hidden rc-overflow-y-auto ${
-          //@ts-ignore
-          headingClasses[`l${headings.length}`]
-        }`}
+        className={`rc-grid rc-grid-cols-7 rc-overflow-hidden rc-overflow-y-auto`}
       >
         {headings.map(day => (
           <div
@@ -94,22 +94,28 @@ export function MonthlyBody<DayData>({
             className={headingClassName}
             aria-label="Day of Week"
           >
-            {day.label}
+            {day.label.substring(0, mobile ? 1 : 3)}
           </div>
         ))}
         {padding.map((_, index) => (
           <div
             key={index}
-            className={headingClassName}
+            className={paddingClassName}
             aria-label="Empty Day"
-          />
+          >
+            {underMonth[index]}
+          </div>
         ))}
         {daysToRender.map(day => (
           <MonthlyBodyContext.Provider
             key={day.toISOString()}
             value={{
               day,
-              events: events.filter((item) => new Date(day) <= new Date(item.endDate) && new Date(item.startDate) <= new Date(day)),
+              events: events.filter(
+                item =>
+                  new Date(day) <= new Date(item.endDate) &&
+                  new Date(item.startDate) <= new Date(day)
+              )
             }}
           >
             {children}
@@ -117,37 +123,44 @@ export function MonthlyBody<DayData>({
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 type MonthlyDayProps<DayData> = {
-  renderDay: (events: DayData[]) => ReactNode;
-  renderClick?: (day: Date) => ReactNode;
-};
-export function MonthlyDay<DayData>({ renderDay, renderClick }: MonthlyDayProps<DayData>) {
-  let { locale } = useMonthlyCalendar();
-  let { day, events } = useMonthlyBody<DayData>();
-  let dayNumber = format(day, 'd', { locale });
+  renderDay: (events: DayData[]) => ReactNode
+  customRender?: (day: Date, dayNumber: string) => ReactNode
+}
+export function MonthlyDay<DayData>({
+  renderDay,
+  customRender
+}: MonthlyDayProps<DayData>) {
+  let { locale } = useMonthlyCalendar()
+  let { day, events } = useMonthlyBody<DayData>()
+  let dayNumber = format(day, 'd', { locale })
 
   return (
-    <div
-      aria-label={`Events for day ${dayNumber}`}
-      className="md:rc-h-36 rc-p-2 rc-border-b-2 rc-border-r-2"
-    >
+    <div aria-label={`Events for day ${dayNumber}`} className="rc-p-2">
       <div className="rc-flex rc-justify-between">
-        <div className='rc-flex rc-flex-row rc-justify-between rc-w-full'>
-          <div className={`rc-font-normal rc-mb-2 rc-text-sm ${isSameDay(day, new Date) ? "rc-bg-blue-500 rc-text-white rc-px-2 rc-rounded" : ""}`}>
-              {parseInt(dayNumber) < 10 ? "0"+dayNumber : dayNumber}
+        {!customRender && (
+          <div className="rc-flex rc-flex-row rc-justify-between">
+            <div
+              className={`rc-font-normal rc-mb-2 md:rc-text-sm sm:rc-text-xs rc-text-xs ${
+                isSameDay(day, new Date())
+                  ? 'rc-bg-blue-500 rc-text-white rc-px-2 rc-rounded'
+                  : ''
+              }`}
+            >
+              {parseInt(dayNumber) < 10 ? '0' + dayNumber : dayNumber}
+            </div>
           </div>
-          {renderClick ? renderClick(day) : ''}
-        </div>
-        <div className="lg:rc-hidden rc-block">
+        )}
+        {/* <div className="lg:rc-hidden rc-block">
           {format(day, 'EEEE', { locale })}
-        </div>
+        </div> */}
       </div>
       <ul className="rc-divide-gray-200 rc-divide-y rc-overflow-hidden rc-max-h-36 md:rc-max-h-24 rc-overflow-y-auto">
-        {renderDay(events)}
+        {customRender ? customRender(day, dayNumber) : renderDay(events)}
       </ul>
     </div>
-  );
+  )
 }
